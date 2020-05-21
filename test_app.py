cd9702f69d593bcf6f944fce0a9a21d4bf531341
RobLOlson <rob.louis@gmail.com>
2020-05-21 14:20:20 -0800

    ~tests use requests

--- test_app.py
+++ test_app.py
@@ -8,8 +8,7 @@ from app import create_app
 from models import setup_db, Expenditure, Category
 import requests
 
-# APP_URL = "http://moneytracklol.herokuapp.com"
-APP_URL = "http://localhost:5000"
+APP_URL = os.environ.get("APP_URL")
 
 ADMIN_TOKEN = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxCa0EyUmJRNWFlYS1lQndPWDRLNiJ9.eyJpc3MiOiJodHRwczovL3JvYmxvdWlzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWFhMGNlMTZiNjliYzBjMTJlNDBlODUiLCJhdWQiOiJtb25leWFwaSIsImlhdCI6MTU5MDAzMTI0OCwiZXhwIjoxNTkwMTE3NjQ4LCJhenAiOiI0MFBzMFE0WnBzd1ljNzF6RjlvVVUzRTVRSE01UHJwaSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmV4cGVuZGl0dXJlcyIsInBhdGNoOmV4cGVuZGl0dXJlcyIsInBvc3Q6ZXhwZW5kaXR1cmVzIiwicmVhZDpleHBlbmRpdHVyZXMiXX0.T3XsY0XqOjRc8sUH7TKcMAuqJTZepxGlI7gfxQn2JXaAsS8qWGwjbju-v-0ZlmLWUeVh1ieYVPjodMjoTJ6mdtfCzUMZjR_MDnhwJ8-k4b4yPbBEt395ka94dtRpiAJy7ldrArHRimvSNh2ZmXPhVU3TI3YM0e7uqZ_f6fhX7wiZAj1ukMwZ6DVxY3Qgg8UXJt7-AKBcaFcCMD5im3GfYuO1pxtAlr3Jvj29keOI0XNqQVsHG6nolH1FDUxgx33kk02gklIHI3wy39pejCX08R-lb7nO81UlbWk21vW_J-ycpSvJHSc-RCQ7DqTI2Z8biRV9xAen87Bs6C9aLYL9CA"
 
@@ -47,11 +46,10 @@ class TriviaTestCase(unittest.TestCase):
         new_name = "".join(random.choices("abcdefghijklmn", k=10))
         new_amount = random.randint(1, 999)
         db_old_size = len(Expenditure.query.all())
-        breakpoint()
         resp = requests.post(APP_URL+"/expenditures", json = {"name": new_name, "amount": new_amount}, headers = {"Authorization": ADMIN_TOKEN})
 
         # resp = self.client().post('/expenditures', json={"name": new_name, "amount": new_amount,})
-        data = json.loads(resp.data)
+        data = resp.json()
         db_new_size = len(Expenditure.query.all())
 
         self.assertEqual(resp.status_code, 200)
